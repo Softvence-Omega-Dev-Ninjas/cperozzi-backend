64b8e12aaad491234d9b488e0e21d337711a0b5d
chamonsheikh121 <studentchamon1212@gmail.com>
2025-12-19 04:06:15 +0600

    updated

--- src/main/main.module.ts
+++ src/main/main.module.ts
@@ -1,6 +1,5 @@
 import { Module } from "@nestjs/common";
-import { OrganizationModule } from './organization/organization.module';
-
+import { OrganizationModule } from "./organization/organization.module";
 
 @Module({
     imports: [OrganizationModule],
--- src/main/organization/organization.service.ts
+++ src/main/organization/organization.service.ts
@@ -1,29 +1,26 @@
-import { Injectable } from '@nestjs/common';
-import { CreateOrganizationDto } from './dto/create-organization.dto';
-import { UpdateOrganizationDto } from './dto/update-organization.dto';
+import { Injectable } from "@nestjs/common";
+import { CreateOrganizationDto } from "./dto/create-organization.dto";
+import { UpdateOrganizationDto } from "./dto/update-organization.dto";
 
 @Injectable()
 export class OrganizationService {
+    create(createOrganizationDto: CreateOrganizationDto) {
+        return;
+    }
 
+    findAll() {
+        return `This action returns all organization`;
+    }
 
+    findOne(id: number) {
+        return `This action returns a #${id} organization`;
+    }
 
-  create(createOrganizationDto: CreateOrganizationDto) {
-    return 
-  }
+    update(id: number, updateOrganizationDto: UpdateOrganizationDto) {
+        return `This action updates a #${id} organization`;
+    }
 
-  findAll() {
-    return `This action returns all organization`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} organization`;
-  }
-
-  update(id: number, updateOrganizationDto: UpdateOrganizationDto) {
-    return `This action updates a #${id} organization`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} organization`;
-  }
+    remove(id: number) {
+        return `This action removes a #${id} organization`;
+    }
 }
