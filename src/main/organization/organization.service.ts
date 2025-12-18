ce7d9aca6cbd1d2c9166f1ebcbcc49e85129cba5
chamonsheikh121 <studentchamon1212@gmail.com>
2025-12-19 04:45:51 +0600

    organization create, find added

--- src/main/organization/organization.service.ts
+++ src/main/organization/organization.service.ts
@@ -1,22 +1,51 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { CreateOrganizationDto } from "./dto/create-organization.dto";
 import { UpdateOrganizationDto } from "./dto/update-organization.dto";
+import { PrismaService } from "@common/prisma/prisma.service";
 
 @Injectable()
 export class OrganizationService {
+    constructor(private readonly prisma: PrismaService) {}
+
     create(createOrganizationDto: CreateOrganizationDto) {
-        return;
+        const organization = this.prisma.organization.create({
+            data: createOrganizationDto,
+        });
+
+        if (!organization) {
+            throw new BadRequestException("Could not create organization");
+        }
+        return organization;
     }
 
-    findAll() {
-        return `This action returns all organization`;
+    findOne(id: string) {
+        const organization = this.prisma.organization.findUnique({
+            where: { id },
+        });
+
+        if (!organization) {
+            throw new BadRequestException("Organization not found");
+        }
+
+        return organization;
     }
 
-    findOne(id: number) {
-        return `This action returns a #${id} organization`;
-    }
+    update(id: string, updateOrganizationDto: UpdateOrganizationDto) {
+        const organization = this.prisma.organization.findUnique({
+            where: { id },
+        });
 
-    update(id: number, updateOrganizationDto: UpdateOrganizationDto) {
+        if (!organization) {
+            throw new BadRequestException("Organization not found");
+        }
+
+        const updateOrganizaiton = this.prisma.organization.update({
+            where: { id },
+            data: updateOrganizationDto,
+        });
+
+        
+
         return `This action updates a #${id} organization`;
     }
 
